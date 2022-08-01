d7fba9ad7b23ecf2d02ff9f406b27e7fdc0cccb5
mgines <mgines@xmltravelgate.com>
2022-08-01 08:05:22 +0200

    add getAccessToken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -26,8 +26,11 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    // Wtf with this user / password
-    this.http.post<LoginResponse>('http://localhost:3000/getLogin', { user: 'test', password: 'test' }).subscribe(
+    const accessToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5UZzBPREJGTVRSQ1JURXdOVFEyUkRBek9UbEJRemN4TkVZMU5EUkRPVVJCTVVNMlJqZzVPQSJ9.eyJodHRwczovL3RyYXZlbGdhdGV4LmNvbS9pYW0iOlt7ImEiOm51bGwsImMiOiJ4dGciLCJnIjpbeyJhIjpudWxsLCJjIjoicGxhdGZvcm0iLCJnIjpbeyJhIjp7ImFsbCI6eyJhbGwiOnsiYWxsIjpbImNydWQxeGEiXX0sInNwZWVkIjp7ImFsbCI6WyIxYSJdfX0sImplZGkiOnsiYWxsIjp7ImFsbCI6WyJyMSJdfX19LCJjIjoiZnJvbnQiLCJnIjpudWxsLCJwIjp7ImlhbSI6eyJncnAiOlsicjEiXSwibWJyIjpbInIxIl19fSwidCI6IlRFQU0ifV0sInAiOm51bGwsInQiOiJURUFNIn0seyJhIjpudWxsLCJjIjoicHJvZHVjdHMiLCJnIjpbeyJhIjpudWxsLCJjIjoiZGlzdHJpYnV0aW9ueCIsImciOlt7ImEiOm51bGwsImMiOiJkaXN0cmlidXRpb254LXZpZXdlcnMiLCJnIjpudWxsLCJwIjp7ImRpc2JhdCI6eyJoYnQiOlsiY3J1ZDEiXX0sImRpc3dlYiI6eyJod2IiOlsiY3J1ZDEiXX19LCJ0IjoiVEVBTSJ9XSwicCI6bnVsbCwidCI6IlRFQU0ifV0sInAiOm51bGwsInQiOiJURUFNIn0seyJhIjp7ImFsbCI6eyJhbGwiOnsiYWxsIjpbImNydWQxeGEiXX19fSwiYyI6InN0YWZmIiwiZyI6bnVsbCwicCI6eyJpYW0iOnsiZ3JwIjpbInIxIl0sIm1iciI6WyJyMSJdfX0sInQiOiJURUFNIn1dLCJwIjpudWxsLCJ0IjoiUk9PVCJ9XSwiaHR0cHM6Ly90cmF2ZWxnYXRleC5jb20vbWVtYmVyX2lkIjoibWlxdWVsLmdpbmVzQHRyYXZlbGdhdGUuY29tIiwiaHR0cHM6Ly94dGcuY29tL2dyb3VwcyI6W10sImh0dHBzOi8veHRnLmNvbS9yb2xlcyI6W10sImh0dHBzOi8veHRnLmNvbS9wZXJtaXNzaW9ucyI6W10sIm5pY2tuYW1lIjoibWdpbmVzIiwibmFtZSI6Im1pcXVlbC5naW5lc0B0cmF2ZWxnYXRlLmNvbSIsInBpY3R1cmUiOiJodHRwczovL3MuZ3JhdmF0YXIuY29tL2F2YXRhci80YmE0OGUzMjEwNjA2MjcwY2Q4MDVjZjNjM2U0OTk5Yz9zPTQ4MCZyPXBnJmQ9aHR0cHMlM0ElMkYlMkZjZG4uYXV0aDAuY29tJTJGYXZhdGFycyUyRm1nLnBuZyIsInVwZGF0ZWRfYXQiOiIyMDIyLTA3LTI5VDA3OjMyOjE4Ljk5NVoiLCJlbWFpbCI6Im1pcXVlbC5naW5lc0B0cmF2ZWxnYXRlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL3h0Zy5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjExZDAwMTg1NzBhMjcwMDcwODBmMGE3IiwiYXVkIjoiejViczdZbzVMNXRaMThoVTdhSHNreVF1N251dHlhZ08iLCJpYXQiOjE2NTkwNzk5NDEsImV4cCI6MTY1OTExNTk0MSwiYXRfaGFzaCI6IjAtQ1g2Zmh3c0t6SV9lTjBzMGhGVGciLCJub25jZSI6Ik5HaDJmM1UxWmtqbjZEZ3o1TTE3ZjJ5MEdNdUp0SkdsIn0.pCrUJGFmRvO5j3EOSjTMbqxptX09LTlhr7YDTSRfGtBboKgCn5m6M0a0vjFqG-wbv6PsYaL1Qvg2gKKzDXq-9Powg7t5yy5zKEOonSvqIagdo-Y31d_BQHKpdiVq_nGbqYrjtGbnYSi79HEepgIflE6IwThSo7QBJWBnAiDufcDV5EpkCtvMErawl20hIF59xeCMfic4Vq81b4b9Yf4cvyt6yeDWVglGRs2Ny64mh8azqxufSppWvA38AzxJntcbqp73WsY7KvU9WtAD0Nph1wznH1mZaIcRLoARspgyJET0oVWPQSTJ3izgEDFGHRQrbk4DZP6n4i2x3Mcgf20F2g'
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('Authorization', 'Bearer ' + accessToken);
+    this.http.get<LoginResponse>('http://localhost:3000/getAccessToken', { headers: headers}).subscribe(
       (response: LoginResponse) => {
         this.userId = response['userId'];
         this.accessToken = response['accessToken'];
